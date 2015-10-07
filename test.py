04b5da7319976b95b9354cda4473c2bb409a55b0
Morro Luarvik <dr.morro.l@gmail.com>
2015-10-07 18:08:27 +1000

    update label value

--- test.py
+++ test.py
@@ -2,10 +2,15 @@
 
 from Tkinter import *
 
-root = Tk()
+def resizeApp(event):
+	click(click)
+	#print('resizeApp')
 
+root = Tk()
 root.title('Test application')
 root.geometry('600x400')
+root.bind('<Configure>', resizeApp)
+
 
 firstRow = Frame(root)
 firstRow.pack(fill=X)
@@ -23,6 +28,15 @@ heightLabel.pack(side=LEFT)
 heightLabelValue = Label(secondRow, text='yyy')
 heightLabelValue.pack(side=LEFT)
 
+def click(event):
+	withLabelValue.config(text=root.winfo_width())
+	heightLabelValue.config(text=root.winfo_height())
+	#print('Action button is clicked')
+	#print(event)
+
+actionButton = Button(root, text='Action!')
+actionButton.bind('<Button-1>', click)
+actionButton.pack()
 
 root.mainloop()
 
