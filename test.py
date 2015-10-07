f928ea903c4c82dbbc9372ecb5876b2e133d2295
morro @ home <dr.morro.l@gmail.com>
2015-10-07 12:48:45 +1000

    place labels

--- test.py
+++ test.py
@@ -7,12 +7,24 @@ root = Tk()
 root.title('Test application')
 root.geometry('600x400')
 
-withLabel = Label(text='Width:')
-withLabel.pack()
-heightLabel = Label(text='Height:')
-heightLabel.pack()
+firstRow = Frame(root)
+firstRow.pack(fill=X)
+
+withLabel = Label(firstRow, text='Width: ')
+withLabel.pack(side=LEFT)
+withLabelValue = Label(firstRow, text='xxx')
+withLabelValue.pack(side=LEFT)
+
+secondRow = Frame(root)
+secondRow.pack(fill=X)
+
+heightLabel = Label(secondRow, text='Height: ')
+heightLabel.pack(side=LEFT)
+heightLabelValue = Label(secondRow, text='yyy')
+heightLabelValue.pack(side=LEFT)
 
 
 root.mainloop()
 
 print("Bye!")
+ 
