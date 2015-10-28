0618b9d561eff0d9e302edf65f2f6fedc0caba63
Morro Luarvik <dr.morro.l@gmail.com>
2015-10-28 22:40:10 +1000

    in class refactoring

--- test.py
+++ test.py
@@ -2,10 +2,53 @@
 
 from Tkinter import *
 
-def resizeApp(event):
-	click(click)
-	#print('resizeApp')
+class MainWindow:
+	def __init__(self):
+		self._createUI()
+		
+	def start(self):
+		self.root.mainloop()
+		
+	def _createUI(self):
+		self.root = Tk()
+		self.root.title('Test application')
+		self.root.geometry('600x400')
+		
+		self.firstRow = Frame(self.root)
+		self.firstRow.pack(fill=X)
 
+		self.withLabel = Label(self.firstRow, text='Width: ')
+		self.withLabel.pack(side=LEFT)
+		self.withLabelValue = Label(self.firstRow, text='xxx')
+		self.withLabelValue.pack(side=LEFT)
+
+		self.secondRow = Frame(self.root)
+		self.secondRow.pack(fill=X)
+
+		self.heightLabel = Label(self.secondRow, text='Height: ')
+		self.heightLabel.pack(side=LEFT)
+		self.heightLabelValue = Label(self.secondRow, text='yyy')
+		self.heightLabelValue.pack(side=LEFT)
+		
+		self.actionButton = Button(self.root, text='Action!')
+		self.actionButton.pack()
+
+		self._setBindings()
+		
+	def _setBindings(self):
+		self.root.bind('<Configure>', self._resizeApp)
+		self.actionButton.bind('<Button-1>', self._click)
+
+	def _resizeApp(self, event):
+		self.withLabelValue.config(text=self.root.winfo_width())
+		self.heightLabelValue.config(text=self.root.winfo_height())
+
+	def _click(self, event):
+		print('click event')
+		#print(event)
+		#click(click)
+	#print('resizeAp')
+"""
 root = Tk()
 root.title('Test application')
 root.geometry('600x400')
@@ -27,18 +70,11 @@ heightLabel = Label(secondRow, text='Height: ')
 heightLabel.pack(side=LEFT)
 heightLabelValue = Label(secondRow, text='yyy')
 heightLabelValue.pack(side=LEFT)
+"""
 
-def click(event):
-	withLabelValue.config(text=root.winfo_width())
-	heightLabelValue.config(text=root.winfo_height())
-	#print('Action button is clicked')
-	#print(event)
-
-actionButton = Button(root, text='Action!')
-actionButton.bind('<Button-1>', click)
-actionButton.pack()
-
-root.mainloop()
+if __name__ == "__main__":
+    mw = MainWindow()
+    mw.start()
 
 print("Bye!")
  
