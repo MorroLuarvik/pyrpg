3fc5ba85fa3ac5221c0e8f3e0cac47b2bf3bd5c5
Mikhail Kozlov <kmo@nkteh.ru>
2015-11-23 17:53:39 +1000

    DIY pseudorandom class

---
+++ rnd/rnd.py
@@ -0,0 +1,18 @@
+class Rnd:
+	a = 211#61211
+	c = 56543
+	seed = 0
+	m = 65537#65521
+	
+	def __init__(self, seed = 0):
+		self.seed = seed
+	
+	def get(self, max = None):
+		self.seed = (self.a * self.seed + self.c) % self.m
+		
+		if not(max):
+			return self.seed
+		if max == 1:
+			return 0
+			
+		return (self.seed * max + 1) // self.m
